d20e19779c0523ac0072720d56c44d1089fe0680
BraydenJenkins <56943172+BraydenJenkins@users.noreply.github.com>
2019-12-20 23:45:45 -0700

    Added Parallax effect and transitions

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ const logo_div = document.getElementById("logo");
 logo_div.addEventListener('click', function(){
     console.log("LOGO CLICKED!");
 })
+
+window.onscroll = function() {changeHeader()};
+function changeHeader() {
+  if (document.body.scrollTop > 590 || document.documentElement.scrollTop > 590) {
+    document.getElementById("tabs").className = "headerBG";
+    document.getElementsByClassName("tab")[0].className = "tabBG";
+  } else {
+    document.getElementById("tabs").className = "";
+    document.getElementsByClassName("tabBG")[0].className = "tab";
+  }
+}
